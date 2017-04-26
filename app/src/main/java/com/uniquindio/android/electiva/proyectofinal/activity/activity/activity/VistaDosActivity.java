e52950f33da0fd638794115a10aeb4ec35a77bd5
jhoneider <jhoneider.mv@gmail.com>
2017-04-26 12:07:35 -0500

    ulltima version 26/04/17 12:07 pm

--- app/src/main/java/com/uniquindio/android/electiva/proyectofinal/activity/activity/activity/VistaDosActivity.java
+++ app/src/main/java/com/uniquindio/android/electiva/proyectofinal/activity/activity/activity/VistaDosActivity.java
@@ -1,10 +1,13 @@
 package com.uniquindio.android.electiva.proyectofinal.activity.activity.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 import com.uniquindio.android.electiva.proyectofinal.R;
+import com.uniquindio.android.electiva.proyectofinal.activity.activity.activity.fragments.AgregarInvestigadorFragment;
 
 /**
  * Created by camilo marin on 25/04/2017.
@@ -16,5 +19,10 @@ public class VistaDosActivity extends AppCompatActivity{
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vista_dos);
+    }
+
+    public void fragmentAgregar(View view){
+        Intent intent = new Intent(this, AgregarInvestigadorFragment.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/uniquindio/android/electiva/proyectofinal/activity/activity/activity/fragments/AgregarInvestigadorFragment.java
+++ app/src/main/java/com/uniquindio/android/electiva/proyectofinal/activity/activity/activity/fragments/AgregarInvestigadorFragment.java
@@ -2,9 +2,11 @@ package com.uniquindio.android.electiva.proyectofinal.activity.activity.activity
 
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RadioButton;
 
 import com.uniquindio.android.electiva.proyectofinal.R;
 
@@ -12,7 +14,10 @@ import com.uniquindio.android.electiva.proyectofinal.R;
  * Created by camilo marin on 25/04/2017.
  */
 
-public class AgregarInvestigadorFragment extends DialogFragment {
+public class AgregarInvestigadorFragment extends DialogFragment implements View.OnClickListener{
+
+    RadioButton radioButtonInvestigador;
+    RadioButton radioButtonGrupo;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -24,6 +29,24 @@ public class AgregarInvestigadorFragment extends DialogFragment {
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+        radioButtonInvestigador =  (RadioButton) getView().findViewById(R.id.rbInsertarInvestigador);
+        radioButtonGrupo =  (RadioButton) getView().findViewById(R.id.rbInsertarGrupoInvestigacion);
 
+        radioButtonInvestigador.setOnClickListener((View.OnClickListener) this);
+        radioButtonGrupo.setOnClickListener((View.OnClickListener) this);
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.rbInsertarInvestigador:
+                Log.v("alerta","Se selecciono el boton de investigador");
+                break;
+            case R.id.rbInsertarGrupoInvestigacion:
+                Log.v("alerta","Se selecciono el boton de grupo de investigación");
+                break;
+        }
     }
 }
