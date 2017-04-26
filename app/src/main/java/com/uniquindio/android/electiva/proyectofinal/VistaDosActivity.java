ae38fe5b101ece3ac7257e22bf669b40274f7d64
jhoneider <jhoneider.mv@gmail.com>
2017-04-25 22:02:01 -0500

    navega entre main_activity y ventana_dos

--- app/src/main/java/com/uniquindio/android/electiva/proyectofinal/VistaDosActivity.java
+++ app/src/main/java/com/uniquindio/android/electiva/proyectofinal/VistaDosActivity.java
@@ -1,8 +1,18 @@
 package com.uniquindio.android.electiva.proyectofinal;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
 /**
  * Created by camilo marin on 25/04/2017.
  */
 
-public class VistaDosActivity {
+public class VistaDosActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vista_dos);
+    }
 }
