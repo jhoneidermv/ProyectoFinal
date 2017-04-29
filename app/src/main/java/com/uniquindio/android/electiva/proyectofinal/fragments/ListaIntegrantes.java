d0e27ac4cab30b47289a8de467e9960f01cdba57
felipeSalcedo <felipesalcedo13@gmail.com>
2017-04-29 11:33:31 -0500

    adaptador

--- app/src/main/java/com/uniquindio/android/electiva/proyectofinal/fragments/ListaIntegrantes.java
+++ app/src/main/java/com/uniquindio/android/electiva/proyectofinal/fragments/ListaIntegrantes.java
@@ -1,5 +1,8 @@
 package com.uniquindio.android.electiva.proyectofinal.fragments;
 
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
+
 import com.uniquindio.android.electiva.proyectofinal.activity.VistaIngresarInvestigador;
 
 import java.util.List;
@@ -8,7 +11,8 @@ import java.util.List;
  * Created by JUAN FELIPE on 29/04/2017.
  */
 
-public class ListaIntegrantes {
+public class ListaIntegrantes extends Fragment {
 
   List<VistaIngresarInvestigador> investigadores;
+    private RecyclerView listadoInvestigadores;
 }
---
+++ app/src/main/java/com/uniquindio/android/electiva/proyectofinal/util/AdaptadorInvestigador.java
@@ -0,0 +1,8 @@
+package com.uniquindio.android.electiva.proyectofinal.util;
+
+/**
+ * Created by JUAN FELIPE on 29/04/2017.
+ */
+
+public class AdaptadorInvestigador {
+}
